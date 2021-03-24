b64b53741dc9851d29a5b59a28e66d0989349ecd
Angelo Dalzotto <angelodalzotto97@gmail.com>
2021-03-24 14:10:47 -0300

    Alterado para padrão do numpy

--- teste.py
+++ teste.py
@@ -2,31 +2,48 @@
 #Nome: Leonardo Erthal
 #Data: 18/03
 #
+
 import numpy as np
-X_SIZE = 8;
-Y_SIZE = 8;
 
-def window_cost(x,y,w_size):
-    x = X_SIZE - x - 1; #Modifica x por causo do numpy
-    z = 0;
-    ate = x - w_size + 1;
-    for aux in range(w_size**2):
-        if (x >= 0 and y <= 7): #7 é o valor maximo de y
-            z = z + m[x][y];
-        else:
-            x = ate;
-            if (y == 7): #7 é o valor maximo y
-                break;
-        print(z)
-        if (x == ate): 
-            x = x + size - 1;
-            y = y + 1;
-        else:
-            x = x - 1;
-    return z;
+APP_TASKS = 9
 
-arr = np.arange(64);
-m = arr.reshape(8,8);
-print(m)
-r = window_cost(6,6,3);
-print(r)
+X_SIZE = 8
+Y_SIZE = 8
+
+PKG_MAX_LOCAL_TASKS = 4
+
+LAST_WINDOW = (1, 1)
+STRIDE = 2
+
+free_page_matrix = np.random.randint(PKG_MAX_LOCAL_TASKS + 1, size=(X_SIZE, Y_SIZE))
+print(free_page_matrix)
+
+def window_cost(x_start, y_start, w_size):
+	free_page_cnt = 0
+
+	x_limit = x_start + w_size
+	if x_limit > X_SIZE:
+		x_limit = X_SIZE
+
+	y_limit = y_start + w_size
+	if y_limit > Y_SIZE:
+		y_limit = Y_SIZE
+
+	for x in range(x_start, x_limit):
+		for y in range(y_start, y_limit):
+			free_page_cnt = free_page_cnt + free_page_matrix[x][y]
+	
+	return free_page_cnt
+
+free_pages_window = 0
+selected_window = (-1, -1)
+w = 3
+
+
+
+
+
+free_pages = window_cost(2, 2, 3)
+print("Free pages = {:d}".format(free_pages))
+
+
