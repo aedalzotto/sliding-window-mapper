3d704456840718716943a9284304776c148f3acb
LeoErthal <leonardo.e@edu.pucrs.br>
2021-04-14 13:18:31 -0300

    build_app adicionado

--- teste.py
+++ teste.py
@@ -15,8 +15,34 @@ PKG_MAX_LOCAL_TASKS = 4
 STRIDE = 2
 LAST_WINDOW = (STRIDE,STRIDE)
 
+#dependence_list = [5,-2,-3,-4,-5]
+dependence_list = [3,2,-3,-3,-1]
+#dependence_list = [3,-3,-3,-1]
+#dependence_list = [3,-3,0,-2]
+
+def build_app(dp_list):
+	tam = len(dp_list) - 1
+	d = []
+	aux = []
+	for x in range(tam):
+		if (dp_list[x + 1] > 0):
+			aux.append(dp_list[x+1]-1)
+		else:
+			aux.append(abs(dp_list[x+1])-1)
+			d.append(aux)
+			aux = []
+	return d
+
+def antecessor_task(dp_list, t):
+	#Retornar uma lista de tarefas antecessoras á tarefa "t"
+	print("oi")
+
+lista = build_app(dependence_list)
+print(lista)
+
+
 free_page_matrix = np.random.randint(PKG_MAX_LOCAL_TASKS + 1, size=(X_SIZE, Y_SIZE))
-print(free_page_matrix)
+#print(free_page_matrix)
 
 def window_cost(x_start, y_start, w_size):
 	free_page_cnt = 0
@@ -48,7 +74,7 @@ while (True):
 	#Alterar a busca para começar a partir da última janela selecionada
 	while (True):
 		free_pages = window_cost(selected_window[0], selected_window[1], w)
-		print(free_pages)
+		#print(free_pages)
 		if (free_pages > free_pages_window):
 			free_pages_window = free_pages
 			if (free_pages_window >= (w**2 * PKG_MAX_LOCAL_TASKS)): #totalmente livre
@@ -65,11 +91,11 @@ while (True):
 			selected_window = (0,0)
 		if (selected_window == LAST_WINDOW):
 			break
-		print(selected_window)
+		#print(selected_window)
 	if (free_pages_window >= APP_TASKS):
 		LAST_WINDOW = selected_window
 		break
 	else:
 		w = w + 1
 		selected_window = (0,0)
-print(free_pages_window)
+#print(free_pages_window)
