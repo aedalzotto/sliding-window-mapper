22089176f05baf76a8710dfb4cf15e3d6da83263
Angelo Dalzotto <angelodalzotto97@gmail.com>
2021-12-20 15:31:15 -0300

    Convert identation to tabs

--- modules/task.py
+++ modules/task.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
 
 class Task:
-    def __init__(self, name, task_id, successors):
-        self.name = name
-        self.id = task_id
-        self.successors = successors
-        self.mapped = (-1, -1)
+	def __init__(self, name, task_id, successors):
+		self.name = name
+		self.id = task_id
+		self.successors = successors
+		self.mapped = (-1, -1)
 
-    def get_id(self):
-        return self.id
+	def get_id(self):
+		return self.id
 
-    def get_name(self):
-        return self.name
+	def get_name(self):
+		return self.name
 
-    def get_successors(self):
-        return self.successors
+	def get_successors(self):
+		return self.successors
 
-    def get_mapped(self):
-        return self.mapped
+	def get_mapped(self):
+		return self.mapped
 
-    def set_mapping(self, pe, cost):
-        self.mapped = pe
-        self.cost = cost
+	def set_mapping(self, pe, cost):
+		self.mapped = pe
+		self.cost = cost
 
-    def set_score(self, score):
-        self.score = score
+	def set_score(self, score):
+		self.score = score
 
-    def get_score(self):
-        return self.score
+	def get_score(self):
+		return self.score
 
-    def get_cost(self):
-        return self.cost
+	def get_cost(self):
+		return self.cost
